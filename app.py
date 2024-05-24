3fa00ea5276ec04d2d0557642f76e5ccc12fe294
kiryaa865 <75220897+kiryaa865@users.noreply.github.com>
2024-05-25 02:02:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -225,6 +225,7 @@ if st.button("Analyze"):
     roberta_pos_tags = tag_roberta_text(input_text)
     
     # OpenAI API interaction
+    assistant = client.beta.assistants.retrieve("asst_7Lbs35tXNgg5HwAkjQKU5xZs")
     user_message = str(input_text)
     thread = client.beta.threads.create()
     message = client.beta.threads.messages.create(thread_id=thread.id, role="user", content=user_message)
