e45c76c30e5eafef5921d94cf6c7ac09eb7405df
kiryaa865 <75220897+kiryaa865@users.noreply.github.com>
2024-05-26 20:18:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,18 @@ from openai import OpenAI
 import subprocess
 import string 
 import torch
-hide_github_icon = """
-
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-"""
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 client = openai.Client(api_key=os.environ.get("OPENAI_API_KEY"))
 # Load secrets
